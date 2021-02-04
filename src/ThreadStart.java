91e9c3a17a91ce52d0562cf640ee91facbbb2524
Braden Hayes <bradenhayes@cmail.carleton.ca>
2021-02-04 11:47:58 -0500

    Added method to start threads

---
+++ src/ThreadStart.java
@@ -0,0 +1,30 @@
+
+public class ThreadStart {
+		/**
+		 *
+		 * 
+		 * @param args
+		 * 
+		 */
+		public static void main(String[] args) {
+			Thread elevator, floor, scheduler;
+			Controller control;
+			
+			control = new Controller();
+			
+			elevator = new Thread (new Elevator(control),"Elevator");
+			scheduler = new Thread (new Scheduler(control),"Scheduler");
+			floor = new Thread (new Floor(control),"Floor");
+			elevator.start();
+			scheduler.start();
+			floor.start();
+			
+			
+		}
+	}
+
+
+
+
+
+
