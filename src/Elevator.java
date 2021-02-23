a9ba0a84134641451dba6f3074be8094910793ee
Braden Hayes <bradenhayes@cmail.carleton.ca>
2021-02-23 18:34:25 -0500

    Just added some logic

--- src/Elevator.java
+++ src/Elevator.java
@@ -4,11 +4,9 @@ import java.util.*;
 public class Elevator implements Runnable{
 	enum State {
 		DOORCLOSED,
-		ACCELERATING,
-		CRUISING,
-		DECELERATING,
-		STOPPED,
-		DOOROPEN
+		DOOROPEN,
+		MOVING,
+		STOPPED
 	}
 	
 
@@ -16,10 +14,13 @@ public class Elevator implements Runnable{
 	 * Shared controller instance, used as the medium to pass data between threads
 	 */
 	private Controller controller;
-	public static State currentState=State.DOOROPEN;
+	public State currentState=State.DOOROPEN;
 	
-	public static long doorCloseTime = 0;
-	public static int test = 1;
+
+	public ArrayList<PersonRequest> response;
+	public ArrayList<Integer> destination;
+	public int CurrentFloor=1;
+	
 	/**
 	 * The Floor constructor initializes an instance of Scheduler and assigns the shared Controller instance
 	 */
@@ -28,27 +29,28 @@ public class Elevator implements Runnable{
 	}
 	@Override
 	public void run() {
-
+		destination = new ArrayList<Integer>();
 		while(true) {
-			ArrayList<PersonRequest> response = controller.getInstructions();
-			System.out.println("4. Requests obtained by Elevator Thread!");
-			controller.putElevatorResponses(response);
-			System.out.println("5. Requests put by Elevator Thread!");
-			try {
-				Thread.sleep(1000);				
-			}
-			catch(InterruptedException e) {}
-		}
-		
-		
-	}
-	public static void stateMachine(){
-		while (true) {			
-			if(test==1) {
+			if(currentState == State.DOOROPEN) {
+				response = controller.getInstructions();
+				System.out.println("4. Requests obtained by Elevator Thread!");
+				if(response.get(0).getFloor() != CurrentFloor){
+					
+					destination.add(response.get(0).getFloor());
+					
+				}
+			
+					destination.add(response.get(0).getCarButton());
+					System.out.println(destination.toString());
+				
+				
 				currentState=State.DOORCLOSED;
-				test =0;
+				
+				System.out.println("              Doors are closing " );
+				
+			
 				try {
-					Thread.sleep(10000);
+					Thread.sleep(1000);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -56,8 +58,20 @@ public class Elevator implements Runnable{
 			}
 			
 			if(currentState == State.DOORCLOSED ) {
-				currentState=State.ACCELERATING;
-				System.out.println("ah");  
+				
+				currentState = State.MOVING;
+				System.out.println("              Elevator is moving");  
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if(currentState == State.MOVING ) {
+				
+				currentState = State.STOPPED;
+				System.out.println("              Elevator has stopped");  
 				try {
 					Thread.sleep(1000);
 				} catch (InterruptedException e) {
@@ -67,49 +81,28 @@ public class Elevator implements Runnable{
 			}
 				
 			
-			if(currentState == State.ACCELERATING) {
-				currentState=State.CRUISING;
-				System.out.println("ahh");  
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-				
-				
-			}
-			if(currentState == State.CRUISING) {
-				currentState=State.DECELERATING;
-				System.out.println("ahhh");  
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-			}
-			if(currentState == State.DECELERATING) {
-				currentState=State.STOPPED;
-				System.out.println("ahhhh");  
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
+			
+			
 			if(currentState == State.STOPPED) {
+				controller.putElevatorResponses(response);
+				System.out.println("5. Requests put by Elevator Thread!");
 				currentState=State.DOOROPEN;
-				System.out.println("ahhhhh");  
+
+				try {
+					Thread.sleep(1000);				
+				}
+				catch(InterruptedException e) {}
+			}
 				
 				
-		} 
 		}
-			
-		}
-	
 }
+}
+		
+			
+		
+	
+
+
+	
 	
