e7b0cd0f4efe3eda7b114d2d52cbcf9565dfb1b8
Malcolm Mcgillivray <71039237+Drake-mcg21@users.noreply.github.com>
2021-04-01 19:55:34 -0400

    Create NEwWindow.java

---
+++ src/NEwWindow.java
@@ -0,0 +1,33 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+
+public class NEwWindow implements ActionListener {
+	JFrame frame = new JFrame();
+	JButton backbutton = new JButton("BACK");
+	NEwWindow(){
+		backbutton.setBounds(0,0,100,50);
+		backbutton.setFocusable(false);
+		frame.add(backbutton);
+		backbutton.addActionListener(this);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(420, 420);
+		frame.setLayout(null);
+		frame.setVisible(true);
+	}
+		public void actionPerformed(ActionEvent e) {
+			String action = e.getActionCommand();
+			if(action.equals("BACK")) {
+				Interface myInterface = new Interface();
+				Interface.main(null);
+				frame.dispose();
+			}
+			}
+			
+		}
+		
+
+
